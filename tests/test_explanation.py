226e8a89fba131118ac18103282b15af3e0c0534
edesz <edesz@users.noreply.github.com>
2026-04-17 13:38:17 -0400

    uncommented tests

--- tests/test_explanation.py
+++ tests/test_explanation.py
@@ -4,78 +4,78 @@
 """Test methods to explain a trained model."""
 
 
-# import numpy as np
-# import pytest
-# from sklearn.base import clone
+import numpy as np
+import pytest
+from sklearn.base import clone
 
-# import src.cc_churn.explanation as expl
-
-
-# def test_get_shap_values_output_types(trained_model):
-#     """Test Basic output structure."""
-#     clf, X_pre = trained_model
-
-#     shap_values, shap_exp = expl.get_shap_values(clf, X_pre)
-
-#     assert isinstance(shap_values, np.ndarray)
-#     assert hasattr(shap_exp, "values")
+import src.cc_churn.explanation as expl
 
 
-# def test_get_shap_values_shape(trained_model):
-#     """Test output shape for both outputs."""
-#     clf, X_pre = trained_model
+def test_get_shap_values_output_types(trained_model):
+    """Test Basic output structure."""
+    clf, X_pre = trained_model
 
-#     shap_values, shap_exp = expl.get_shap_values(clf, X_pre)
+    shap_values, shap_exp = expl.get_shap_values(clf, X_pre)
 
-#     assert shap_values.shape == X_pre.shape
-#     assert shap_exp.values.shape == X_pre.shape
-
-
-# def test_shap_outputs_not_identical(trained_model):
-#     """Test that outputs are not identical."""
-#     clf, X_pre = trained_model
-
-#     shap_values, shap_exp = expl.get_shap_values(clf, X_pre)
-
-#     assert not np.testing.assert_array_equal(shap_values, shap_exp.values)
+    assert isinstance(shap_values, np.ndarray)
+    assert hasattr(shap_exp, "values")
 
 
-# def test_feature_order_preserved(trained_model):
-#     """Test that expected number of features are returned."""
-#     clf, X_pre = trained_model
+def test_get_shap_values_shape(trained_model):
+    """Test output shape for both outputs."""
+    clf, X_pre = trained_model
 
-#     _, shap_exp = expl.get_shap_values(clf, X_pre)
+    shap_values, shap_exp = expl.get_shap_values(clf, X_pre)
 
-#     assert shap_exp.values.shape[1] == X_pre.shape[1]
-
-
-# def test_subset_input(trained_model):
-#     """Test that SHAP works on a subset of the features."""
-#     clf, X_pre = trained_model
-
-#     X_subset = X_pre.iloc[:10]
-
-#     shap_values, shap_exp = expl.get_shap_values(clf, X_subset)
-
-#     assert shap_values.shape[0] == 10
-#     assert shap_exp.values.shape[0] == 10
+    assert shap_values.shape == X_pre.shape
+    assert shap_exp.values.shape == X_pre.shape
 
 
-# def test_array_input_type(trained_model):
-#     """Test that a numpy array can be provided as input."""
-#     clf, _ = trained_model
-#     X_array = np.random.rand(10, 5)
+def test_shap_outputs_not_identical(trained_model):
+    """Test that outputs are not identical."""
+    clf, X_pre = trained_model
 
-#     shap_values, shap_exp = expl.get_shap_values(clf, X_array)
-#     assert shap_values.shape[0] == X_array.shape[0]
-#     assert shap_exp.values.shape[1] == X_array.shape[1]
+    shap_values, shap_exp = expl.get_shap_values(clf, X_pre)
+
+    assert not np.testing.assert_array_equal(shap_values, shap_exp.values)
 
 
-# def test_unfitted_model_raises(trained_model):
-#     """Test behaviour on untrained model."""
-#     clf, X_pre = trained_model
+def test_feature_order_preserved(trained_model):
+    """Test that expected number of features are returned."""
+    clf, X_pre = trained_model
 
-#     clf_untrained = clone(clf)
+    _, shap_exp = expl.get_shap_values(clf, X_pre)
 
-#     with pytest.raises(TypeError):
-#         expl.get_shap_values(clf_untrained, X_pre)
+    assert shap_exp.values.shape[1] == X_pre.shape[1]
+
+
+def test_subset_input(trained_model):
+    """Test that SHAP works on a subset of the features."""
+    clf, X_pre = trained_model
+
+    X_subset = X_pre.iloc[:10]
+
+    shap_values, shap_exp = expl.get_shap_values(clf, X_subset)
+
+    assert shap_values.shape[0] == 10
+    assert shap_exp.values.shape[0] == 10
+
+
+def test_array_input_type(trained_model):
+    """Test that a numpy array can be provided as input."""
+    clf, _ = trained_model
+    X_array = np.random.rand(10, 5)
+
+    shap_values, shap_exp = expl.get_shap_values(clf, X_array)
+    assert shap_values.shape[0] == X_array.shape[0]
+    assert shap_exp.values.shape[1] == X_array.shape[1]
+
+
+def test_unfitted_model_raises(trained_model):
+    """Test behaviour on untrained model."""
+    clf, X_pre = trained_model
+
+    clf_untrained = clone(clf)
+
+    with pytest.raises(TypeError):
+        expl.get_shap_values(clf_untrained, X_pre)
--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -1,57 +1,57 @@
-# #!/usr/bin/env python3
+#!/usr/bin/env python3
 
 
-# """Test prediction scorers."""
+"""Test prediction scorers."""
 
 
-# import numpy as np
-# import pytest
+import numpy as np
+import pytest
 
-# import src.cc_churn.scoring as sc
-
-
-# def test_get_scorers_keys():
-#     """Test get_scorers with valid keys."""
-#     scorers = sc.get_scorers(["f2", "recall", "accuracy"])
-#     assert set(scorers.keys()) == {"f2", "recall", "accuracy"}
+import src.cc_churn.scoring as sc
 
 
-# def test_get_scorers_invalid_key():
-#     """Test get_scorers with invalid key."""
-#     with pytest.raises(KeyError):
-#         sc.get_scorers(["invalid_metric"])
+def test_get_scorers_keys():
+    """Test get_scorers with valid keys."""
+    scorers = sc.get_scorers(["f2", "recall", "accuracy"])
+    assert set(scorers.keys()) == {"f2", "recall", "accuracy"}
 
 
-# def test_get_scorers_callable():
-#     """Test get_scorers for a callable."""
-#     scorers = sc.get_scorers(["f1"])
-#     scorer = scorers["f1"]
-#     assert callable(scorer)
+def test_get_scorers_invalid_key():
+    """Test get_scorers with invalid key."""
+    with pytest.raises(KeyError):
+        sc.get_scorers(["invalid_metric"])
 
 
-# def test_scorer_execution(binary_data):
-#     """Test scorer fu nctionality."""
-#     y_true, y_pred, _ = binary_data
-#     scorers = sc.get_scorers(["accuracy", "recall", "f1"])
-#     results = {
-#         k: scorer._score_func(y_true, y_pred, **scorer._kwargs)
-#         for k, scorer in scorers.items()
-#     }
-#     assert np.isclose(results["accuracy"], 0.5)
-#     assert np.isclose(results["recall"], 0.5)
-#     assert np.isclose(results["f1"], 0.5)
+def test_get_scorers_callable():
+    """Test get_scorers for a callable."""
+    scorers = sc.get_scorers(["f1"])
+    scorer = scorers["f1"]
+    assert callable(scorer)
 
 
-# def test_prauc_scorer(binary_data):
-#     """Test prauc behavior."""
-#     y_true, y_pred, _ = binary_data
-#     scorers = sc.get_scorers(["prauc"])
-#     result = scorers["prauc"]._score_func(y_true, y_pred)
-#     assert 0.0 <= result <= 1.0
+def test_scorer_execution(binary_data):
+    """Test scorer fu nctionality."""
+    y_true, y_pred, _ = binary_data
+    scorers = sc.get_scorers(["accuracy", "recall", "f1"])
+    results = {
+        k: scorer._score_func(y_true, y_pred, **scorer._kwargs)
+        for k, scorer in scorers.items()
+    }
+    assert np.isclose(results["accuracy"], 0.5)
+    assert np.isclose(results["recall"], 0.5)
+    assert np.isclose(results["f1"], 0.5)
 
 
-# def test_rocauc_scorer(binary_data):
-#     y_true, _, y_pred_proba = binary_data
-#     scorers = sc.get_scorers(["rocauc"])
-#     result = scorers["rocauc"]._score_func(y_true, y_pred_proba)
-#     assert 0.0 <= result <= 1.0
+def test_prauc_scorer(binary_data):
+    """Test prauc behavior."""
+    y_true, y_pred, _ = binary_data
+    scorers = sc.get_scorers(["prauc"])
+    result = scorers["prauc"]._score_func(y_true, y_pred)
+    assert 0.0 <= result <= 1.0
+
+
+def test_rocauc_scorer(binary_data):
+    y_true, _, y_pred_proba = binary_data
+    scorers = sc.get_scorers(["rocauc"])
+    result = scorers["rocauc"]._score_func(y_true, y_pred_proba)
+    assert 0.0 <= result <= 1.0
